4c0da713a3f8d31dd525d8ec43f9756b43eeca28
Igorexy <iak7@bk.ru>
2022-09-17 22:56:57 +0300

    Added resized widgets

--- functional.py
+++ functional.py
@@ -17,3 +17,24 @@ def db_connect():
         print("Подключено к базе данных", db_name)
     return db
 
+
+def gridLayoutStartResize():
+    """Начальное изменение размера сетки для дальнего шего нормального ресайза окна.
+    Связано с тем, что gridLayout не получается сделать больше начального значения"""
+    parameter_search = '   <widget class="QWidget" name="gridLayoutWidget">\n    <property name="geometry">\n     <rect>\n      <x>0</x>\n      <y>0</y>\n      <width>'
+    GUI_file = open('MainForm.ui', 'r')
+    file_info = ''
+    for line in GUI_file:
+        file_info += line
+    inx = file_info.find(parameter_search)     # начальный индекс параметра
+    inx_end = file_info.find('\n     </rect>', inx)   # конечный индекс настройки grid layout
+    setting = file_info[inx:inx_end]
+    # замена размеров для последующего уменьшения
+    file_info = file_info.replace(setting,
+    '   <widget class="QWidget" name="gridLayoutWidget">\n    <property name="geometry">\n     <rect>\n      <x>0</x>\n      <y>0</y>\n      <width>8000</width>\n      <height>6000</height>'
+    )
+    GUI_file.close()
+    GUI_file = open('MainFormResize.ui', 'w')
+    GUI_file.write(file_info)
+    GUI_file.close()
+
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+from multiprocessing.sharedctypes import Value
 from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication
 from functional import *
+from threading import Thread
+from time import sleep
 
-Form, Window = uic.loadUiType("MainForm.ui")
+gridLayoutStartResize()     # изменнение размеров основного слоя в MainForm для корректного изменения размеров виджетов
+
+Form, Window = uic.loadUiType("MainFormResize.ui")
 
 # Настройка интерфейса
 app = QApplication([])
@@ -23,11 +28,25 @@ def output_table():
     form.tableView.setModel(VUZ)
     form.tableView.setSortingEnabled(True)
 
+end_of_work = False
+def widget_resize():
+    """Функция для изменения размеров виджетов вместе с размером окна"""
+    while True:
+        geometry = form.centralwidget.geometry()
+        form.gridLayout.setGeometry(geometry)
+        sleep(0.1)
+        if end_of_work: return
+Thread(target=widget_resize).start()
 
+#Thread(target=widget_resize).start()
+print(form.gridLayout.maximumSize())
 # взаимодействие с интерфесом
 form.output_db.clicked.connect(output_table)
+#form.pushButton.clicked.connect(widget_resize)
 
 
 # Запуск приложения
 window.show()
 app.exec()
+
+end_of_work = True
