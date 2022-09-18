83c8ce19a6048578b7c5b7b9c688458613dced95
Igorexy <iak7@bk.ru>
2022-09-18 16:11:08 +0300

    try fixed thread resize but not today

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from multiprocessing.sharedctypes import Value
 from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication
 from functional import *
 from threading import Thread
 from time import sleep
+from PyQt6.QtCore import QThread
 
-gridLayoutStartResize()     # изменнение размеров основного слоя в MainForm для корректного изменения размеров виджетов
+gridLayoutStartResize()     # изменнение размеров основного слоя gridLayout в MainForm для корректного изменения размеров виджетов
 
 Form, Window = uic.loadUiType("MainFormResize.ui")
 
@@ -29,17 +29,24 @@ def output_table():
     form.tableView.setSortingEnabled(True)
 
 end_of_work = False
-def widget_resize():
-    """Функция для изменения размеров виджетов вместе с размером окна"""
-    while True:
-        geometry = form.centralwidget.geometry()
-        form.gridLayout.setGeometry(geometry)
-        sleep(0.1)
-        if end_of_work: return
-Thread(target=widget_resize).start()
 
-#Thread(target=widget_resize).start()
-print(form.gridLayout.maximumSize())
+
+class ResizeThread(QThread):
+    """Класс для изменения размеров виджетов вместе с размером окна"""
+    def __init__(self, parent=None):
+        super(ResizeThread, self).__init__(parent)
+
+    def run(self):
+        while True:
+            if end_of_work: return
+            geometry = form.centralwidget.geometry()
+            form.gridLayout.setGeometry(geometry)
+            sleep(0.05)
+# с кнопкой почему-то не выдает "Timers cannot be started from another thread", а со слоем выдает
+# менять каждый виджет отдельно, как решение, но это нужно ещё один алгоритм писать, пока что лень
+resizeThread = ResizeThread()
+resizeThread.start()
+
 # взаимодействие с интерфесом
 form.output_db.clicked.connect(output_table)
 #form.pushButton.clicked.connect(widget_resize)
@@ -49,4 +56,4 @@ form.output_db.clicked.connect(output_table)
 window.show()
 app.exec()
 
-end_of_work = True
+end_of_work = True  # для завершения потока
